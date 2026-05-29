8bdd4f3826e932f06618b285df52ad36cf4d21ee
zeena-create <eai25094@alshaab.edu.iq>
2026-05-29 16:37:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -249,4 +249,5 @@ def on_invite_friend(data):
 if __name__ == '__main__':
     os.makedirs('templates', exist_ok=True)
     os.makedirs('static', exist_ok=True)
-    socketio.run(app, host='0.0.0.0', port=5000, debug=False)
+    port = int(os.environ.get('PORT', 5000))
+socketio.run(app, host='0.0.0.0', port=port, debug=False)
